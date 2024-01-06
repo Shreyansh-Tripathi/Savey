e054215344877a944b79e158b854b22e64a25fee
Shreyansh-Stage <shreyansh@stage.in>
2024-01-06 19:21:43 +0530

    ui updates

--- lib/my_app.dart
+++ lib/my_app.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:provider/provider.dart';
 import 'package:savey/ui/provider/provider.dart';
 import 'package:savey/ui/screen/goal_detail_screen.dart';
@@ -8,8 +9,15 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
+      statusBarColor: Color(0xff2D2C79), // Change this to the desired color
+      statusBarIconBrightness:
+          Brightness.light, // Use Brightness.dark for dark icons
+    ));
     return  MaterialApp(
-      theme: ThemeData(fontFamily: 'Montserrat'),
+      theme: ThemeData(
+        fontFamily: 'Montserrat',
+      ),
       debugShowCheckedModeBanner: false,
       home: ChangeNotifierProvider(
         create: (context) => GoalProvider(),
