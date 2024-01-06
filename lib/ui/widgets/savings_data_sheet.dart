b5e04df054c4502f16ec722e84154a2cca5f32ce
Shreyansh-Stage <shreyansh@stage.in>
2024-01-06 21:12:51 +0530

    ui updates

--- lib/ui/widgets/savings_data_sheet.dart
+++ lib/ui/widgets/savings_data_sheet.dart
@@ -43,7 +43,7 @@ class SavingsDataSheet extends StatelessWidget {
             ),
           ),
           Container(
-            padding: const EdgeInsets.symmetric(horizontal: 25),
+            padding: const EdgeInsets.symmetric(horizontal: 50),
             height: 10.0,
             width: double.infinity,
             child: Row(
@@ -60,7 +60,7 @@ class SavingsDataSheet extends StatelessWidget {
             ),
           ),
           Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+            padding: const EdgeInsets.symmetric(horizontal: 40, vertical: 20),
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.center,
               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
